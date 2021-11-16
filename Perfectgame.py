2337d9d89d5cc6cb51ff4865c8451a10e7ab0e1c
Theerthana D K <93856522+Theerthana22@users.noreply.github.com>
2021-11-16 22:55:09 +0530

    project1

---
+++ Perfectgame.py
@@ -0,0 +1,28 @@
+import random
+randNumber = random.randint(1,70)
+print(randNumber)
+userGuess = None
+guesses = 0
+
+while(userGuess != randNumber):
+    userGuess = int(input("Enter your guess: "))
+    guesses += 1
+    if(userGuess==randNumber):
+        print("You guessed it right!")
+    else:
+        if(userGuess>randNumber):
+            print("You guessed it wrong! Enter a smaller number")
+        else:
+            print("You guessed it wrong! Enter a larger number")
+
+    #guesses += 1
+
+print(f"You guessed the number in {guesses} guesses")
+with open("hiscore.txt", "r") as f:
+    hiscore = int(f.read())
+
+if(guesses<hiscore):
+    print("You have just broken the high score!")
+    with open("hiscore.txt", "w") as f:
+        f.write(guesses)
+        
